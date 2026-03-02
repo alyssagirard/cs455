9014043283a8547c5f6cbbd74f6ff326e30889c3
Miller <mill25jx@stu.cs.jmu.edu>
2026-03-02 13:46:16 -0500

    updated testing

--- testing/test.sh
+++ testing/test.sh
@@ -5,13 +5,16 @@ echo ""
 echo "- - - TEST 1 - - - - -"
 echo ""
 ../p1 inputs/trafficArpIcmp.pcap | diff - expected/expectedOutput1.txt
+../p1 inputs/trafficArpIcmp.pcap > outputs/1.txt
 
 echo ""
 echo "- - - TEST 2 - - - - -"
 echo ""
 ../p1 inputs/trafficIPoptions.pcap | diff - expected/expectedOutput2.txt
+../p1 inputs/trafficIPoptions.pcap > outputs/2.txt
 
 echo ""
 echo "- - - TEST 3 - - - - -"
 echo ""
 ../p1 inputs/trafficUdpTcp.pcap | diff - expected/expectedOutput3.txt
+../p1 inputs/trafficUdpTcp.pcap > outputs/3.txt
